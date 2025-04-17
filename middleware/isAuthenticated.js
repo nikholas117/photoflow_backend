3223b742675e1e19a26d41a0200e90456fbb29e6
nikholas117 <nikholas123@hotmail.com>
2025-04-17 00:03:10 -0400

    fixing

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,25 +1,35 @@
 const jwt = require("jsonwebtoken");
-const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
 const User = require("../models/userModel");
 
 const isAuthenticated = catchAsync(async (req, res, next) => {
+  // Get token from cookies or headers
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
   if (!token) {
     return next(
       new AppError("You are not logged in! Please log in to access.", 401)
     );
   }
-  console.log(req.headers.authorization?.split(" ")[1]);
 
+  // Verify the token
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  if (!decoded) {
+    return next(new AppError("Invalid token. Please log in again.", 401));
+  }
+
+  // Find user by decoded id from token
   const currentUser = await User.findById(decoded.id);
 
-  if (!currentUser)
+  if (!currentUser) {
     return next(
       new AppError("The user belonging to this token does not exist.", 401)
     );
-  req.user = currentUser;
+  }
+
+  req.user = currentUser; // Attach the user to the request object
 
   next();
 });
